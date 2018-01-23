52a65a962c8f409e2296b73187beed4973ccf256
city0409 <250924125@qq.com>
2018-01-23 10:32:45 +0800

    0123AM，地面不动

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour 
+{
+    [SerializeField]
+    private Transform player;
+
+    private Vector3 dis;
+    private Vector3 tempPos;
+    [SerializeField]
+    private LayerMask mask;
+
+    //private Transform mainCamera;
+    private void Awake () 
+	{
+        //mainCamera = GetComponent<Transform>();
+        dis = player.position - transform.position;
+    }
+	
+	private void LateUpdate () 
+	{
+        tempPos = transform.position;
+        //transform.position = Vector3.Lerp(player.position - dis, transform .position , Time.deltaTime * 5);
+        //if (Physics2D.Raycast(player.position, Vector2.down,2f)) {
+        //    Debug.Log(hit.point);
+        //}
+        RaycastHit2D hit = Physics2D.Raycast(player.position, Vector2.down, 10f, mask);
+        tempPos = Vector2.Lerp(tempPos, hit.point + new Vector2(10,-5), Time.deltaTime * 5);
+        tempPos.z = -10;
+        transform.position = tempPos;
+
+
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,12 +7,17 @@ public enum State { Idle, Jump }
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
-    private float speed = 2f;
+    private Collider BGcoll;
+    [SerializeField]
+    private float maxSpeed = 2f;
+    [SerializeField]
+    private float force = 10f;
     [SerializeField]
     private float jumpForce = 200f;
 
     private Rigidbody2D rig;
     private State currentState = State.Idle;
+    private float speed;
 
     private void Awake()
     {
@@ -23,31 +28,49 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0) && currentState == State.Idle)
         {
-            currentState = State.Jump;
-        }
-        if (  currentState == State.Jump )
-        {
-            currentState = State.Idle;
             StartCoroutine(Jump());
         }
-        if (Input.GetMouseButtonDown(1) && currentState == State.Jump)
-        {
-            StartCoroutine(Torque());
+        
+        //if (Input.GetMouseButtonDown(1) && currentState == State.Jump)
+        //{
+        //    StartCoroutine(Torque());
 
-        }
+        //}
+    }
+
+    private void FixedUpdate()
+    {
+        //rig.velocity = new Vector2(speed, rig.velocity.y);
+        rig.AddForce(new Vector2(force, -force), ForceMode2D.Force);
+
+        speed = Mathf.Clamp(Vector3.SqrMagnitude(rig.velocity), 0f, maxSpeed);
+        rig.velocity = rig.velocity.normalized * speed;
     }
 
     private IEnumerator Jump()
     {
-        rig.AddForce(new Vector2(1, 1) * jumpForce);
+        rig.AddForce(new Vector2(1f, 1f) * jumpForce,ForceMode2D.Impulse);
+        currentState = State.Jump;
 
-        yield return null;
+        yield return new WaitForSeconds (0.5f);
     }
 
     private IEnumerator Torque()
     {
         rig.AddTorque(10f, ForceMode2D.Force);
 
-        yield return null;
+        yield return new WaitForSeconds(0.5f);
+    }
+
+    private void OnCollisionEnter2D(Collision2D BGcoll)
+    {
+        if (BGcoll.gameObject .tag =="BG")
+        {
+            if (currentState == State.Jump)
+            {
+                currentState = State.Idle;
+
+            }
+        }
     }
 }
